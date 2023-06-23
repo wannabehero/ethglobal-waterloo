6ec81759acfc27dac780120ac63f1878dcdda201
Bayram Annakov <bayram.annakov@gmail.com>
2023-06-23 19:38:57 -0400

    implemented getEbayItem by url

--- backend/src/ebay/ebay.controller.ts
+++ backend/src/ebay/ebay.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { EbayService } from './ebay.service';
 
 @Controller('ebay')
 export class EbayController {
   constructor(private readonly svc: EbayService) {}
+
+  @Get('getEbayItem')
+  async getEbayItem(@Query('itemUrl') itemUrl: string): Promise<any> {
+    console.log(`in ebay controller: ${itemUrl}`);
+    return this.svc.getEbayItem(itemUrl);
+  }
+
 }
--- backend/src/ebay/ebay.service.ts
+++ backend/src/ebay/ebay.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
 
+
 @Injectable()
-export class EbayService {}
+export class EbayService {
+
+
+    async getEbayItem(itemUrl: string): Promise<string> {
+        const response = await fetch('https://api.apify.com/v2/acts/dtrungtin~ebay-items-scraper/run-sync-get-dataset-items?token=apify_api_Mu3Gri5HGx2TYBpgWInZIL5VjxNfws3YVkMU', {
+            method: 'POST',
+            body: JSON.stringify({
+                "maxItems": 1,
+                "proxyConfig": {
+                    "useApifyProxy": true
+                },
+                "startUrls": [
+                    {
+                        "url": itemUrl
+                    },
+                ]
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            });
+            const data = await response.text();
+            return data;
+
+    }
+}
