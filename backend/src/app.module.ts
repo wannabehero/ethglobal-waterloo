866d716589cab0df3b1794d7e8c6db6a396edaa1
Bayram Annakov <bayram.annakov@gmail.com>
2023-06-24 00:03:42 -0400

    fedex module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ReputationModule } from './reputation/reputation.module';
 import { EbayModule } from './ebay/ebay.module';
 import { ZkModule } from './zk/zk.module';
+import { FedexModule } from './fedex/fedex.module';
 
 @Module({
-  imports: [ReputationModule, EbayModule, ZkModule],
+  imports: [ReputationModule, EbayModule, ZkModule, FedexModule],
   controllers: [],
   providers: [],
 })
