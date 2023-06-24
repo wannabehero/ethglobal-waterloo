9151ed2f5bf8bf28c299a80c88a4782a08a4f864
Bayram Annakov <bayram.annakov@gmail.com>
2023-06-23 21:09:51 -0400

    added initial airtstack integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { ReputationModule } from './reputation/reputation.module';
 import { EbayModule } from './ebay/ebay.module';
 
+import { init} from "@airstack/airstack-react";
+
+const AIRSTACK_API_KEY = "3119c035cfa24462b8cca0ef4a89772f";
+
+init(AIRSTACK_API_KEY);
+
 @Module({
   imports: [ReputationModule, EbayModule],
   controllers: [],
--- backend/src/reputation/reputation.service.ts
+++ backend/src/reputation/reputation.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@nestjs/common';
 
+import { fetchQuery } from "@airstack/airstack-react";
+
 @Injectable()
 export class ReputationService {
 
     async getReputation(walletAddress: string): Promise<string> {
         console.log(`in reputation service: ${walletAddress}`);
+
+        const query = `{
+            TokenBalances(
+              input: {filter: {owner: {_in: ["${walletAddress}"]}, tokenType: {_in: [ERC1155, ERC721]}}, blockchain: ethereum, limit: 10}
+            ) {
+              TokenBalance {
+                tokenAddress
+                amount
+                formattedAmount
+                tokenType
+                owner {
+                  addresses
+                }
+                tokenNfts {
+                  address
+                  tokenId
+                  blockchain
+                  contentValue {
+                    image {
+                      original
+                    }
+                  }
+                }
+              }
+            }
+          }`;
+
+        const { data, error } = await fetchQuery(query);
+
+        data.TokenBalances.TokenBalance.forEach((tokenBalance) => {
+            console.log(tokenBalance);
+        });
+        
+
         return '0.5';
     }
 
